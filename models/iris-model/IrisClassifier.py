ab141a46a911bdf628d50efaf2cd766e090672aa
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-07-11 11:29:20 +1000

    Initial Checkin

---
+++ models/iris-model/IrisClassifier.py
@@ -0,0 +1,10 @@
+import joblib
+
+class IrisClassifier(object):
+
+    def __init__(self):
+        self.model = joblib.load('IrisClassifier.sav')
+        self.class_names = ["iris-setosa","iris-vericolor","iris-virginica"];
+
+    def predict(self,X,features_names):
+        return self.model.predict_proba(X)
